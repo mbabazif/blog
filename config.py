2e78fca7a26e6cb833df392cb27f26cd6f45821f
Florence <mbababziflorence81@gmail.com>
2019-03-02 16:36:42 +0200

    adding the subscription file

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ import os
 
 
 class Config:
+    QUOTES_API_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:mba123456@localhost/blog'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # export MOVIE_API_KEY='7854ce99b646526f11604daf32dc1202'
 export SECRET_KEY='0788504294ma0788896659pa'
-export MAIL_USERNAME='mbabaziflorence07@gmail.com'
-export MAIL_PASSWORD='0787624774abc'
+export MAIL_USERNAME=mbabaziflorence81@gmail.com
+export MAIL_PASSWORD=mba123456
 python3.6 manage.py server
